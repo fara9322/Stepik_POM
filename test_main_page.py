66f6e64ecc4f6c771d3ecd3183eb6d719aa55722
fara9322 <80336129+fara9322@users.noreply.github.com>
2021-07-05 13:31:40 +0300

    assert for ulr link, registration form and login form step 4.2 (point 8)

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
 from pages.main_page import MainPage
-
+from pages.login_page import LoginPage
 
 
 def test_guest_should_see_login_link(browser):
@@ -7,3 +7,21 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_for_login_or_reg(browser):
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_quest_should_see_login_form(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_quest_should_see_registration_form(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
