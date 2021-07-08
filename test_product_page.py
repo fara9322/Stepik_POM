aec67c3a10bce485709b518e67b5956ebf368bcc
fara9322 <80336129+fara9322@users.noreply.github.com>
2021-07-08 10:39:33 +0300

    Задание: наследование и отрицательные проверки (test_guest_cant_see_product_in_basket_opened_from_product_page)

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 #import time
 import pytest
 from pages.product_page import ProductPage
-
+from pages.basket_page import BasketPage
 
 def test_user_can_add_product_to_basket(browser):
     links = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -67,6 +67,15 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser,link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_cart()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.basket_is_empty()
+    basket_page.basket_null_text()
 
 
 
